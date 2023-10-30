cf8b04974e4cd343941eee247242d0ee39afe23e
Guilherme <guillhermmme@gmail.com>
2023-10-30 11:19:09 -0300

    json

--- endpointLista.php
+++ endpointLista.php
@@ -1,34 +1,32 @@
 <?php
 
+    $JsonAlertas = '{
+       
+        "0": {
+            "nome": "Guilherme",
+            "setor": "P&D",
+            "ramal": 35
+       },
+       
+       "1" : {
+        "nome": "Murilo",
+        "setor": "P&D",
+        "ramal": 37
+       },
 
-$lista_alertas = [];
+       "2" : {
+        "nome": "Lucas",
+        "setor": "P&D",
+        "ramal": 40
+       },
 
-    $arrayLista = array(
-       "0" => array(
-            "nome"=> "Guilherme",
-            "setor"=> "P&D",
-            "ramal"=> 35
-        ),
-       "1" => array(
-        "nome"=> "Murilo",
-        "setor"=> "P&D",
-        "ramal"=> 37
-       ),
-       "2" => array(
-        "nome"=> "Lucas",
-        "setor"=> "P&D",
-        "ramal"=> 40
-       ),
-       "3" => array(
-        "nome"=> "Matheus",
-        "setor"=> "P&D",
-        "ramal"=> 41
-       ),              
+       "3" : {
+        "nome": "Matheus",
+        "setor": "P&D",
+        "ramal": 41
+       },              
 
-    );
+    }';
 
-    array_push($lista_alertas, $arrayLista);
-
-
-    echo json_encode($lista_alertas);
+    echo $JsonAlertas;
 
