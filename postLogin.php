c9e8b96b95988db5645e34ecd1185946fd618e93
Guilherme Nunes <guillhermmme@gmail.com>
2023-12-12 17:06:55 -0300

    login

---
+++ postLogin.php
@@ -0,0 +1,49 @@
+<?php
+
+header('Content-Type: application/json');
+//JSON_UNESCAPED_UNICODE
+
+if (isset($_REQUEST['login'])) {
+    if (!empty($_REQUEST['login'])) {
+        $login = $_REQUEST['login'];
+    } else {
+        http_response_code(401);
+        print_r(json_encode("Erro! O conteúdo de login não pode estar vazio!"));
+        die();
+    }
+} else {
+    http_response_code(401);
+    print_r(json_encode("Erro! Você precisa enviar o login!"));
+    die();
+}
+
+if (isset($_REQUEST['senha'])) {
+    if (!empty($_REQUEST['senha'])) {
+        $senha = $_REQUEST['senha'];
+    } else {
+        http_response_code(401);
+        print_r(json_encode("Erro! O conteúdo de login não pode estar vazio!"));
+        die();
+    }
+} else {
+    http_response_code(401);
+    print_r(json_encode("Erro! Você precisa enviar o login!"));
+    die();
+}
+
+
+
+$mensagem = array(
+    "login" => $login,
+    "titulo" => 'Teste API Login e Senha',
+    "response" => $nome." login realizado com sucesso.",
+    "data" => date("d/m/Y H:i")
+);
+
+print_r(json_encode($mensagem));
+
+
+
+
+
+
